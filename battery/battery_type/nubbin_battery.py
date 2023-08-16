acde567ea0d234a0bad5bb1a1eed46af22a7f62e
MehakKambo <mehakpreetp12@gmail.com>
2023-08-16 12:30:39 -0700

    Done refactoring the codebase

---
+++ battery/battery_type/nubbin_battery.py
@@ -0,0 +1,17 @@
+from battery import Battery
+
+
+
+class NubbinBattery(Battery):
+    
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+    
+    def needs_service(self):
+        date_need_service = self.last_service_date.replace(year=self.last_service_date + 3)
+        if date_need_service < self.current_date:
+            return True
+        else:
+            return False
+        
